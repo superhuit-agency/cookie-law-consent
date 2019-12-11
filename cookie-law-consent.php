5179c7d764a1d65197bfc3499f57008110dce093
Felipe <felipe.paulmartins@outlook.com>
2019-12-11 16:39:09 +0100

    initial commit

---
+++ cookie-law-consent.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Cookie Law Consent
+ * Description: Handle your Cookies and give the user the ability to Accept or not the cookies.
+ * Version:     1.0.0
+ * Author URI:  https://profiles.wordpress.org/kuuak
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: clc
+ * Domain Path: cookielawconsent
+ *
+ * Cookie Law Consent is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * Cookie Law Consent is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @package Woo_Default_Attributes
+ * @author  Kuuak
+ * @license GPL-2.0+
+ * @link    https://github.com/Kuuak/cookie-law-consent
+ */
+
+namespace CookieLawConsent;
+
+/* Prevent loading this file directly */
+defined( 'ABSPATH' ) || exit;
