1fc7988c4af82008838a81bd27b96ff7ce92ac5b
Felipe <felipe.paulmartins@outlook.com>
2019-12-20 17:12:19 +0100

    Make sure the categories config is an array

--- public/index.js
+++ public/index.js
@@ -3,4 +3,7 @@ import CookieLaw from './src/cookie-law';
 
 const config = ( typeof clc_config === 'undefined' ) ? {} : JSON.parse(clc_config);
 
+if ( typeof config.categories === 'object' )
+	config.categories = Object.keys(config.categories).map(key => config.categories[key]);
+
 new CookieLaw(config);
