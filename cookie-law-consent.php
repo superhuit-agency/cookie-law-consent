1bd2ca3b334842d65d4c71a544dd6bf53285348e
Charlotte Habre <charlotte@superhuit.ch>
2022-03-17 14:07:34 +0100

    update version in files

--- cookie-law-consent.php
+++ cookie-law-consent.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Cookie Law Consent
  * Description: Handle your Cookies and give the user the ability to Accept or not the cookies.
- * Version:     2.0.1
+ * Version:     2.0.2
  * Author:  superhuit
  * Author URI:  https://profiles.wordpress.org/superhuit
  * License:     GPL-2.0+
