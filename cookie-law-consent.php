aa2e94bf105cf46129dce6cf3cbeca17e148b243
Felipe <felipe@superhuit.ch>
2022-06-27 17:39:50 +0200

    v1.4.1 ✨

--- cookie-law-consent.php
+++ cookie-law-consent.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Cookie Law Consent
  * Description: Handle your Cookies and give the user the ability to Accept or not the cookies.
- * Version:     1.4.0
+ * Version:     1.4.1
  * Author:  superhuit
  * Author URI:  https://profiles.wordpress.org/superhuit
  * License:     GPL-2.0+
