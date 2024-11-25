e99ef68d7817ca4233246a4ba8e17b29da14cad7
Felipe Paul Martins <felipe@superhuit.ch>
2024-11-25 14:44:29 +0100

    chore: :arrow_up: upgrade dependencies & restructure plugin files
    - node v20
    - webpack v5
    - move all files into src folder
    - Lint
    - Add wp & polylang deps for DX

---
+++ src/public/assets/services/index.js
@@ -0,0 +1,23 @@
+/**
+ * The available services are heavily
+ * insppired/copied from tarteaucitron.js
+ * and adapted to our needs
+ *
+ * @see https://github.com/AmauriC/tarteaucitron.js/blob/master/tarteaucitron.services.js
+ */
+
+import googleanalytics from "./googleanalytics";
+import googletagmanager from "./googletagmanager";
+import facebookpixel from "./facebookpixel";
+import recaptcha from "./recaptcha";
+import googlemaps from "./googlemaps";
+import pardot from "./pardot";
+
+export default {
+	googleanalytics,
+	googletagmanager,
+	facebookpixel,
+	recaptcha,
+	googlemaps,
+	pardot,
+};
